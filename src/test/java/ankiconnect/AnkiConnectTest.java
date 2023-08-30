213e8a1596979608c89b952f5c8b7c055180780c
Casey Scarborough <caseyscarborough@gmail.com>
2023-08-30 15:17:44 -0400

    Add unit tests with response caching

---
+++ src/test/java/ankiconnect/AnkiConnectTest.java
@@ -0,0 +1,57 @@
+package ankiconnect;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// Tests were ran with a new Anki profile with the following deck:
+// https://ankiweb.net/shared/info/2009196675
+//
+class AnkiConnectTest {
+
+    private final AnkiConnect connect = new AnkiConnect(new TestHttpClient());
+
+    @Test
+    void testFindCards() {
+        final List<Long> cards = connect.findCards("deck:*");
+        assertFalse(cards.isEmpty());
+        assertTrue(cards.contains(1354715263726L));
+    }
+
+    @Test
+    void testFindCardsWithEmptyQuery() {
+        final List<Long> cards = connect.findCards("");
+        assertFalse(cards.isEmpty());
+        assertTrue(cards.contains(1354715263726L));
+    }
+
+    @Test
+    void testFindCardsWithNullQuery() {
+        final List<Long> cards = connect.findCards(null);
+        assertTrue(cards.isEmpty());
+    }
+
+    @Test
+    void testCardsInfo() {
+        final List<Card> cards = connect.cardsInfo(Collections.singletonList(1354715263726L));
+        assertEquals(1, cards.size());
+
+        final Card card = cards.get(0);
+        assertEquals(1354715263726L, card.getCardId());
+    }
+
+    @Test
+    void testReviewsForCards() {
+        final Long id = 1354715263726L;
+        final Map<String, List<Review>> map = connect.getReviewsOfCards(Collections.singletonList(id));
+        assertEquals(1, map.size());
+        final List<Review> reviews = map.get(id.toString());
+        assertEquals(2, reviews.size());
+    }
+}
---
+++ src/test/java/ankiconnect/TestHttpClient.java
@@ -0,0 +1,48 @@
+package ankiconnect;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Slf4j
+class TestHttpClient extends AnkiConnectHttpClient {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public TestHttpClient() {
+        super("localhost", 8765, null);
+    }
+
+    @Override
+    public <P, R> AnkiConnectResponse<R> request(String action, Map<String, P> params, TypeReference<AnkiConnectResponse<R>> token) {
+        try {
+            AnkiConnectRequest<P> body = new AnkiConnectRequest<>();
+            body.setAction(action);
+            body.setVersion(6);
+            body.setParams(params);
+            final String s = mapper.writeValueAsString(body);
+            final String hash = DigestUtils.md5Hex(s);
+            final String relativePath = "src/test/resources/json/" + hash + ".json";
+            final Path path = Paths.get(relativePath);
+            if (Files.exists(path)) {
+                log.info("Reading cached response from {}", relativePath);
+                return mapper.readValue(Files.readAllBytes(path), token);
+            }
+            final AnkiConnectResponse<R> response = super.request(body, token);
+            final File file = path.toFile();
+            file.getParentFile().mkdirs();
+            log.info("Caching response to {}", relativePath);
+            mapper.writeValue(file, response);
+            return response;
+        } catch (Exception e) {
+            throw new AnkiConnectException("Could not make request to the AnkiConnect API", e);
+        }
+    }
+}
